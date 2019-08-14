4d207ff1e7274d50ddaa5c5d2a2bfcc0796ca969
hanyun <1355081829@qq.com>
2019-08-14 10:17:42 +0800

    修改模型

--- models/models.go
+++ models/models.go
@@ -15,28 +15,22 @@ var (
 )
 
 type Model struct {
-	ID        uint64    `json:"id" gorm:"primary_key" form:"ID" `
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	ID        uint64 `json:"id" gorm:"primary_key" form:"ID" `
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
 }
 
-func Default() Model {
-	return Model{CreatedAt: time.Now(), UpdatedAt: time.Now()}
-}
 func DB() *gorm.DB {
 	return db
-}
-func (model *Model) GetById() {
-	db.First(model, model.ID)
 }
 
 func migrate() {
 	models := []interface{}{
-		User{Model: Default()},
+		User{},
 		Admin{},
 		Auth{},
-		Product{Model: Default()},
+		Product{},
 	}
 	db.AutoMigrate(models...)
 }
@@ -62,9 +56,6 @@ func Setup() {
 	}
 	//migrate()// 不让他自动创建表，因为float不可以为无符号型的数据
 	//db.SingularTable(true) // 如果设置为true,`User`的默认表名为`user`,使用`TableName`设置的表名不受影响
-	db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
-	db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
-	db.Callback().Delete().Replace("gorm:delete", deleteCallback)
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
 	go migrate()
@@ -73,67 +64,4 @@ func Setup() {
 // CloseDB closes database connection (unnecessary)
 func CloseDB() {
 	defer db.Close()
-}
-
-// updateTimeStampForCreateCallback will set `CreatedOn`, `ModifiedOn` when creating
-func updateTimeStampForCreateCallback(scope *gorm.Scope) {
-	if !scope.HasError() {
-		nowTime := time.Now().Unix()
-		if createTimeField, ok := scope.FieldByName("CreatedOn"); ok {
-			if createTimeField.IsBlank {
-				createTimeField.Set(nowTime)
-			}
-		}
-
-		if modifyTimeField, ok := scope.FieldByName("ModifiedOn"); ok {
-			if modifyTimeField.IsBlank {
-				_ = modifyTimeField.Set(nowTime)
-			}
-		}
-	}
-}
-
-// updateTimeStampForUpdateCallback will set `ModifiedOn` when updating
-func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
-	if _, ok := scope.Get("gorm:update_column"); !ok {
-		_ = scope.SetColumn("ModifiedOn", time.Now().Unix())
-	}
-}
-
-// deleteCallback will set `DeletedOn` where deleting
-func deleteCallback(scope *gorm.Scope) {
-	if !scope.HasError() {
-		var extraOption string
-		if str, ok := scope.Get("gorm:delete_option"); ok {
-			extraOption = fmt.Sprint(str)
-		}
-
-		deletedOnField, hasDeletedOnField := scope.FieldByName("DeletedOn")
-
-		if !scope.Search.Unscoped && hasDeletedOnField {
-			scope.Raw(fmt.Sprintf(
-				"UPDATE %v SET %v=%v%v%v",
-				scope.QuotedTableName(),
-				scope.Quote(deletedOnField.DBName),
-				scope.AddToVars(time.Now().Unix()),
-				addExtraSpaceIfExist(scope.CombinedConditionSql()),
-				addExtraSpaceIfExist(extraOption),
-			)).Exec()
-		} else {
-			scope.Raw(fmt.Sprintf(
-				"DELETE FROM %v%v%v",
-				scope.QuotedTableName(),
-				addExtraSpaceIfExist(scope.CombinedConditionSql()),
-				addExtraSpaceIfExist(extraOption),
-			)).Exec()
-		}
-	}
-}
-
-// addExtraSpaceIfExist adds a separator
-func addExtraSpaceIfExist(str string) string {
-	if str != "" {
-		return " " + str
-	}
-	return ""
 }
